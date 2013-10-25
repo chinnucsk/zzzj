286b4e245cb9c0cf1b77cd1dd294da8fe36494c2
Shree Kavi <shreekavi@gmail.com>
2013-10-25 16:30:01 +0530

    Add news Page

---
+++ src/news_page_handler.erl
@@ -0,0 +1,33 @@
+-module(news_page_handler).
+-author("shree@ybrantdigital.com").
+
+-export([init/3]).
+
+-export([content_types_provided/2]).
+-export([welcome/2]).
+-export([terminate/3]).
+
+%% Init
+init(_Transport, _Req, []) ->
+	{upgrade, protocol, cowboy_rest}.
+
+%% Callbacks
+content_types_provided(Req, State) ->
+	{[		
+		{<<"text/html">>, welcome}	
+	], Req, State}.
+
+terminate(_Reason, _Req, _State) ->
+	ok.
+
+%% API
+welcome(Req, State) ->
+	{[{Name,Value}], Req2} = cowboy_req:bindings(Req),
+	Url = zzzj_util:news_db_url(binary_to_list(Value)), 
+	{ok, "200", _, Response} = ibrowse:send_req(Url,[],get,[],[]),
+	Res = string:sub_string(Response, 1, string:len(Response) -1 ),
+	Params = jsx:decode(list_to_binary(Res)),
+	{ok, Body} = news_page_dtl:render(Params),
+    {Body, Req2, State}.
+
+
